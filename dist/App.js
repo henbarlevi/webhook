0ef45b96ffa155e815cb131578c75bc99bfa3a18
henbarlevi <hen101020@gmail.com>
2017-09-13 16:45:06 +0300

    m

--- dist/App.js
+++ dist/App.js
@@ -30,7 +30,7 @@ class App {
         /* This is just to get up and running, and to make sure what we've got is
          * working so far. This function will change when we start to add more
          * API endpoints */
-        this.express.use('/', appRoutes_1.default);
+        this.express.use('/api', appRoutes_1.default);
     }
 }
 exports.default = new App().express; //export instance of new app
--- src/App.ts
+++ src/App.ts
@@ -42,7 +42,7 @@ class App {
     /* This is just to get up and running, and to make sure what we've got is
      * working so far. This function will change when we start to add more
      * API endpoints */
-    this.express.use('/', appRoutes);
+    this.express.use('/api', appRoutes);
   }
 
 
